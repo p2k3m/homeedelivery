37da1374aa9f3f8ba423574ec8f6985a724aee6a
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2020-05-24 21:04:33 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
             <button onClick={openMenu}>
               &#9776;
         </button>
-            <Link to="/" >amazona</Link>
+            <Link to="/" >homedelivery</Link>
           </div>
           <div className="header-links">
             <a href="cart.html">Cart</a>
